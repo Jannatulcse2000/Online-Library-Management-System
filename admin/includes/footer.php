caa6f3f61d3c99ed1ae39ac9f1d84fb0c2616b30
Jannatul Ferdus <jannatulcse01@gmail.com>
2024-09-20 03:31:39 +0600

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,14 @@
+<section class="footer-section">
+        <div class="container">
+        <div class="row">
+					<div class="col-md-12 text-center">
+						<p>
+							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Created by <a href="https://www.linkedin.com/">Jannatul </a> & <a href="https://www.linkedin.com/"> Imran,</a> SFMU
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></span> 
+							
+						</p>
+					</div>
+				</div>
+        </div>
+    </section>
